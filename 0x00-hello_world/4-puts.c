1f1739d25ad53d231aadd44410065daf32073ce0
lumi56 <adapelumi156@gmail.com>
2022-04-25 11:00:46 -0700

    hello world tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-
+/**
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle \n");
-
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
